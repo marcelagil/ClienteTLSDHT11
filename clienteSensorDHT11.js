0c254e06aa272114da1c1f4fabf9b2a35d1eff3d
marcelagil <marcela.gil@usach.cl>
2020-11-22 03:22:58 -0300

    Se agrega contador que termina el programa al llegar al numero de mediciones necesarias
    Se agregan campos al sensor y se agrega el campo dispositivo

--- clienteSensorDHT11.js
+++ clienteSensorDHT11.js
@@ -5,6 +5,7 @@
 
 var sensor = require('node-dht-sensor');
 var seqNo = 0;
+var maxMediciones=100;
 setInterval(function () {
 sensor.read(11, 4, function(error, temperature, humidity) {
     if (!error) {
@@ -14,7 +15,13 @@ sensor.read(11, 4, function(error, temperature, humidity) {
         c1.on('connect', function (err) {
             console.log('Cliente conectado-sensor conectado');
             seqNo += 1;
-             //setInterval(function () {
+				if (seqNo>maxMediciones) {
+					console.log("Medicion 100");
+					process.exit();
+				}
+				else {
+					console.log(seqNo);
+				}
                 var medicion= {
                     "sensor" :{
 						"id":1,
@@ -56,18 +63,20 @@ sensor.read(11, 4, function(error, temperature, humidity) {
    
         console.log('STARTED');
     }
-    else {
-       
-        //console.log("l40");
-
+    else {      
+        seqNo=+1;
         console.error("sin señall sensor l45",error);
-                    //module.exports=medicion;
-        //var signOnMsg = medicion;
         var TLSClient = require('./tls-client.js');
         var c1 = new TLSClient('agent1', 8000);        
         c1.on('connect', function (err) {
         console.log('Cliente conectado -- sensor desconectado');
-        //setInterval(function () {
+			if (seqNo>maxMediciones) {
+				console.log("Medicion 100");
+				process.exit();
+			}
+			else {
+					console.log(seqNo);
+			}
             var medicion= {
                 "sensor" :{
 					"id":1,
@@ -87,27 +96,14 @@ sensor.read(11, 4, function(error, temperature, humidity) {
             };
             console.log("segNo: ", medicion); 
             c1.write (medicion);
-            var seqNo=+1;
-            //}, 100);
-
         });
                
         c1.on('disconnect', function (err) {
             console.log('Cliente desconectado');
         });              
-       /*c1.on('message', function (message) {  
-            console.log("Tag = ", message.tag);
-            console.log("Date = ", message.date);
-            //if (message.seqNo !== seqNo) {
-                //console.log ("Sequence number error, expected: ", seqNo);
-                //process.exit();
-            //}
-            if ((message.seqNo % 100) === 0) {
-                console.log (process.memoryUsage());
-            }
-        });*/
+
             console.log('STARTED');
-                //});
+
         }
 });
 }, 6000); 
